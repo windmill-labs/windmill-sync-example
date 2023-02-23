3c5b23ba7dd9e91cf19f95a750c2edc10332b764
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-23 17:57:25 +0100

    test

--- .wmill/f/examples/forloop_sleep.ts
+++ .wmill/f/examples/forloop_sleep.ts
@@ -3,7 +3,7 @@ import { sleep } from "https://deno.land/x/sleep/mod.ts";
 export async function main() {
   for (let i = 0; i < 20; i++) {
     console.log("sleeping...");
-    await sleep(0.6);
+    await sleep(0.65);
   }
-  return "woke up";
+  return "woke up from slumber";
 }
