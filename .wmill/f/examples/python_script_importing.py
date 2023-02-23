2ebc939eecf524d47a95f5117baedd367464d55d
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-23 17:22:22 +0100

    demo

---
+++ .wmill/f/examples/python_script_importing.py
@@ -0,0 +1,5 @@
+from f.examples.python_script_imported import to_be_imported
+
+#See https://app.windmill.dev/scripts/get/f/examples/python_script_imported
+def main(x: str):
+  return to_be_imported(x)
