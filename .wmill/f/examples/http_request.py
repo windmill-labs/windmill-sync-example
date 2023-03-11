dee61880a5797abf40fd2140af161a634ecbe70e
rubenfiszel <rubenfiszel@users.noreply.github.com>
2023-03-11 17:06:29 +0000

    Apply automatic changes

--- .wmill/f/examples/http_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def main(
-    my_value: str
-):
-    headers={'Authorization': "Bearer: {}".format(os.environ.get("WM_USERNAME"))}
-    r = requests.post('https://httpbin.org/post', headers=headers, data={'key': my_value})
-    return r.json()
