64d4e7141a5cff5363e26543d72ce2e2362abdfd
rubenfiszel <rubenfiszel@users.noreply.github.com>
2023-03-11 17:04:38 +0000

    Apply automatic changes

--- f/examples/python_script_importing.py
+++
@@ -1,5 +0,0 @@
-from f.examples.python_script_imported import to_be_imported
-
-#See https://app.windmill.dev/scripts/get/f/examples/python_script_imported
-def main(x: str):
-  return to_be_imported(x)
