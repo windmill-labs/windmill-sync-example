4a3036517341492dcfee438690dfed3d6aa498b0
rubenfiszel <rubenfiszel@users.noreply.github.com>
2023-05-03 14:45:45 +0000

    Apply automatic changes

--- .wmill/f/examples/python_script_importing.py
+++ .wmill/f/examples/python_script_importing.py
@@ -1,7 +1,4 @@
-# Those alternatives would also work
-# from ..examples.python_script_imported import to_be_imported
-# from f.examples.python_script_imported import to_be_imported
-from .python_script_imported import to_be_imported
+from f.examples.python_script_imported import to_be_imported
 
 #See https://app.windmill.dev/scripts/get/f/examples/python_script_imported
 def main(x: str):
