e697e76a6a7d4d798ecad01d35977d3b60909453
ruben <ruben@rubenfiszel.com>
2024-09-09 13:26:01 +0000

    [WM] Script 'f/user/unassailable_script' deployed

--- f/user/unassailable_script.ts
+++ f/user/unassailable_script.ts
@@ -3,7 +3,7 @@ import * as wmill from "windmill-client"
 
 // fill the type, or use the +Resource type to get a type-safe reference to a resource
 // type Postgresql = object
-//
+// foo
 export async function main(
   a: number,
   b: "my" | "enum",
