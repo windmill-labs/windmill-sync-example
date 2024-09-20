61051459917bf8f262abba47c3f8cdb60875d909
admin <admin@windmill.dev>
2024-09-20 08:55:57 +0200

    [WM] Script 'f/utilities/well_regarded_script' deployed

--- f/utilities/well_regarded_script.ts
+++ f/utilities/well_regarded_script.ts
@@ -2,7 +2,7 @@
 // https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
 
 FontFaceSetLoadEventw
-ff
+fff
 // import { toWords } from "number-to-words@1"
 import * as wmill from "windmill-client"
 
