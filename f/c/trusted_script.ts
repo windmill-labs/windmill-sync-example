8ec548cc4c4abbbe93d655ac0507a1a8b658982e
ruben <ruben@rubenfiszel.com>
2025-01-26 09:08:04 +0000

    [WM] Script 'f/c/trusted_script' deployed

--- f/c/trusted_script.ts
+++ f/c/trusted_script.ts
@@ -3,10 +3,10 @@ import * as wmill from "windmill-client"
 
 // fill the type, or use the +Resource type to get a type-safe reference to a resource
 // type Postgresql = object
-
+//f
 export async function main(
   a: number,
-  b: "my" | "enum",
+  b: "myyy" | "enum",
   //c: Postgresql,
   //d: wmill.S3Object, // for large files backed by S3 (https://www.windmill.dev/docs/core_concepts/persistent_storage/large_data_files)
   e = "inferred type string from default arg",
